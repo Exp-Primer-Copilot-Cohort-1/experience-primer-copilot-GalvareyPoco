36a2758f4bf8d094009059583d98887197faaa8b
Galvarey <61221866+GalvareyPoco@users.noreply.github.com>
2024-04-13 18:41:53 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,33 +1,51 @@
-// create web server
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
 var fs = require('fs');
 
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
-app.use(bodyParser.json());
+// Parse JSON bodies of requests
+app.use(express.json());
 
-// handling GET request
+// GET request to retrieve comments
 app.get('/comments', function(req, res){
     fs.readFile('comments.json', function(err, data){
-        res.setHeader('Content-Type', 'application/json');
-        res.send(data);
+        if (err) {
+            console.error('Error reading comments:', err);
+            res.status(500).send('Error reading comments');
+            return;
+        }
+        res.json(JSON.parse(data));
     });
 });
 
-// handling POST request
+// POST request to add a new comment
 app.post('/comments', function(req, res){
-    fs.readFile('comments.json', function(err, data){
-        var comments = JSON.parse(data);
-        comments.push(req.body);
-        fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err){
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(comments));
-        });
+    fs.readFile('comments.json', function(readErr, data){
+        if (readErr) {
+            console.error('Error reading comments:', readErr);
+            res.status(500).send('Error reading comments');
+            return;
+        }
+        try {
+            var comments = JSON.parse(data);
+            comments.push(req.body);
+            fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(writeErr){
+                if (writeErr) {
+                    console.error('Error writing comments:', writeErr);
+                    res.status(500).send('Error writing comments');
+                    return;
+                }
+                res.status(201).json(comments);
+            });
+        } catch (parseErr) {
+            console.error('Error parsing JSON:', parseErr);
+            res.status(400).send('Invalid JSON format');
+        }
     });
 });
 
-// start server
+// Start the server
 var server = app.listen(3000, function(){
     console.log('Server is running at http://localhost:3000/');
 });
